fa1b8c592d37d87917f7dd23f36b84997957ef10
pghernandez <36212966+pghernandez@users.noreply.github.com>
2019-01-31 22:08:29 -0500

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -14,14 +14,14 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
 	var myName = prompt('Please enter your name.');
 	localStorage.setItem('name', myName);
-	myHeading.textContent = 'Mozilla is awesome, ' + myName;
+	myHeading.textContent = 'Welcome ' + myName + '!';
 }
 
 if(!localStorage.getItem('name')) {
 	setUserName();
 } else {
 	var storedName = localStorage.getItem('name');
-	myHeading.textContent = 'Mozilla is awesome, ' + storedName;
+	myHeading.textContent = 'Welcome ' + storedName + '!';
 }
 
 myButton.onclick = function() {
